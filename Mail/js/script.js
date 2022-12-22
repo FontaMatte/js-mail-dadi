884f4b4450fa9667ef5dc12669ddf83cec38eaeb
FontaMatte <matteo.fontanel@gmail.com>
2022-12-22 15:39:12 +0100

    Basic mail exercise completed

---
+++ Mail/js/script.js
@@ -0,0 +1,36 @@
+// 1.CHIEDO ALL'UTENTE LA MAIL 
+// 2.VERIFICO SE LA MAIL E' PRESENTE ALL'INTERNO DI UNA MAILING LIST
+// 3.STAMPO L'ESITO DELLA VERIFICA
+ 
+// uso prompt per chiedere la mail
+const userMail = prompt("Qual'e' il tuo indirizzo mail? (indirizzo per essere verificato: pinco.pallo@gmail.com");
+console.log(userMail);
+
+// Variabile per verificare se l'email è autorizzata
+let emailAuthorized = false;
+
+// genero un array per simulare la mailing list
+const mailingList = ['mail1','mail2','mail3','pinco.pallo@gmail.com','mail4',];
+console.log(mailingList)
+
+// utilizzo un ciclo 'for' per confrontare la mail inserita con quelle presenti nell-array
+for (let i = 0 ; i < mailingList.length ; i++) {
+
+if (userMail == mailingList[i]) {
+    emailAuthorized = true;
+    break;
+}
+}
+
+// stampo i risultati
+if (emailAuthorized == true) {
+
+alert(`Benvenuto, ${userMail}!`);
+
+}
+
+else {
+
+alert('Accesso negato: email non autorizzata.');
+
+}
